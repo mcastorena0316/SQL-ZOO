d5f10677cca503eaaea7613ea8ad00cf8291228a
Melissa Castorena <mcastorena0316@gmail.com>
2019-12-05 14:54:53 -0600

    correction self join exercise 10

--- 9_self_join.sql
+++ 9_self_join.sql
@@ -50,9 +50,10 @@ SELECT stopb.name, a.company, a.num
     WHERE stopa.name='Craiglockhart';
 
 --10)Find the routes involving two buses that can go from Craiglockhart to Lochend. Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus. 
-SELECT  a.num, a.company, stopb.name, d.num, d.company FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num) JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-  JOIN stops stopc ON (c.stop=stopc.id)
-JOIN stops stopd ON (d.stop=stopd.id)
-  WHERE stopa.name = 'Craiglockhart' AND stopd.name = 'Lochend' AND  stopb.name = stopc.name;
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE stopa.name = 'Craiglockhart'
+  AND stopd.name = 'Lochend'
+  AND stopb.name = stopc.name
+ORDER BY b.num, LENGTH(a.num), LENGTH(a.num),stopc.id, c.num
